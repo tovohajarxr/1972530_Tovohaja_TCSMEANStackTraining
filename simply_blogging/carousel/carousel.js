421bc5adc56d999fd9394305628716c694f7d5c0
Tovohaja <hajaranthony@gmail.com>
2021-03-18 13:43:53 -0400

    deleted useless carousel folder

--- simply_blogging/carousel/carousel.js
+++
@@ -1,9 +0,0 @@
-function switchStyle() {
-    if (document.getElementById('styleSwitch').checked) {
-      document.getElementById('gallery').classList.add("custom");
-      document.getElementById('exampleModal').classList.add("custom");
-    } else {
-      document.getElementById('gallery').classList.remove("custom");
-      document.getElementById('exampleModal').classList.remove("custom");
-    }
-}
