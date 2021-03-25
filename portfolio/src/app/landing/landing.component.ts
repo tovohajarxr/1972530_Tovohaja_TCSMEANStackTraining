c010886ad5e6f23ce2a6cee8e9c6646493975825
Tovohaja <hajaranthony@gmail.com>
2021-03-25 19:17:15 -0400

    checkpoint: bulk design

--- portfolio/src/app/landing/landing.component.ts
+++ portfolio/src/app/landing/landing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var M: any; /* Materialize tabs */
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -10,7 +12,10 @@ export class LandingComponent implements OnInit {
 
   constructor(router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { /* Materialize tabs */
+    const elem = document.querySelector('.tabs');
+    const options = {};
+    M.Tabs.init(elem, options);
   }
 
 }
